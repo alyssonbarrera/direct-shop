4f53078046bd4551893efa3e9ff2eea73606e702
Alysson Barrera <alyssonbarrera@outlook.com>
2025-10-18 14:28:40 -0300

    💄 update ui

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,10 +1,51 @@
 import { Text } from '@/core/components/ui/text';
+import { ScrollView } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function ProfileScreen() {
   return (
-    <SafeAreaView>
-      <Text>Profile Screen</Text>
+    <SafeAreaView className="flex-1 bg-background">
+      <ScrollView
+        className="px-6"
+        contentContainerClassName="pb-8"
+        showsVerticalScrollIndicator={false}>
+        <Text className="text-center text-xl font-bold">Profile Screen</Text>
+        <Text className="mt-4 text-base font-medium leading-relaxed">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti non ex saepe veritatis.
+          Eligendi repellendus quis odit officiis dolore et veniam quas odio beatae, exercitationem
+          dignissimos sequi atque aliquid reiciendis.
+        </Text>
+        <Text className="mt-4 text-base font-medium leading-relaxed">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti non ex saepe veritatis.
+          Eligendi repellendus quis odit officiis dolore et veniam quas odio beatae, exercitationem
+          dignissimos sequi atque aliquid reiciendis.
+        </Text>
+        <Text className="mt-4 text-base font-medium leading-relaxed">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti non ex saepe veritatis.
+          Eligendi repellendus quis odit officiis dolore et veniam quas odio beatae, exercitationem
+          dignissimos sequi atque aliquid reiciendis.
+        </Text>
+        <Text className="mt-4 text-base font-medium leading-relaxed">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti non ex saepe veritatis.
+          Eligendi repellendus quis odit officiis dolore et veniam quas odio beatae, exercitationem
+          dignissimos sequi atque aliquid reiciendis.
+        </Text>
+        <Text className="mt-4 text-base font-medium leading-relaxed">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti non ex saepe veritatis.
+          Eligendi repellendus quis odit officiis dolore et veniam quas odio beatae, exercitationem
+          dignissimos sequi atque aliquid reiciendis.
+        </Text>
+        <Text className="mt-4 text-base font-medium leading-relaxed">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti non ex saepe veritatis.
+          Eligendi repellendus quis odit officiis dolore et veniam quas odio beatae, exercitationem
+          dignissimos sequi atque aliquid reiciendis.
+        </Text>
+        <Text className="mt-4 text-base font-medium leading-relaxed">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti non ex saepe veritatis.
+          Eligendi repellendus quis odit officiis dolore et veniam quas odio beatae, exercitationem
+          dignissimos sequi atque aliquid reiciendis.
+        </Text>
+      </ScrollView>
     </SafeAreaView>
   );
 }
